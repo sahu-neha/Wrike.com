7e1fec991aa8f714d60133c47c85da97f7fae95b
Neha <nehasahu2227@gmail.com>
2022-11-11 00:15:43 +0530

    day-2

---
+++ index.js
@@ -0,0 +1,42 @@
+let gridP = document.querySelectorAll(".grid>div>p");
+let divGrid = document.querySelectorAll(".grid>div");
+let gridH3 = document.querySelectorAll(".grid>div>h3");
+
+divGrid[0].onmouseover = () => {
+	gridP[0].style.color = "white";
+	gridH3[0].style.color = "rgb(140, 240, 155)";
+};
+
+divGrid[0].onmouseout = () => {
+	gridP[0].style.color = "#2b3857";
+	gridH3[0].style.color = "rgb(9, 86, 86)";
+};
+
+divGrid[1].onmouseover = () => {
+	gridP[1].style.color = "white";
+	gridH3[1].style.color = "rgb(140, 240, 155)";
+};
+
+divGrid[1].onmouseout = () => {
+	gridP[1].style.color = "#2b3857";
+	gridH3[1].style.color = "rgb(9, 86, 86)";
+};
+
+divGrid[2].onmouseover = () => {
+	gridP[2].style.color = "white";
+	gridH3[2].style.color = "rgb(140, 240, 155)";
+};
+
+divGrid[2].onmouseout = () => {
+	gridP[2].style.color = "#2b3857";
+	gridH3[2].style.color = "rgb(9, 86, 86)";
+};
+
+divGrid[3].onmouseover = () => {
+	gridP[3].style.color = "white";
+	gridH3[3].style.color = "rgb(140, 240, 155)";
+};
+divGrid[3].onmouseout = () => {
+	gridP[3].style.color = "#2b3857";
+	gridH3[3].style.color = "rgb(9, 86, 86)";
+};
